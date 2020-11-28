cf2d615881f17407f62221d085e6b04315868033
stephaniesawyer1020 <stephanie.sawyer@bryanuniversity.edu>
2020-11-28 00:43:25 -0700

    Pause and Practice

---
+++ week2/CreatingElementsandAppendingElements.js
@@ -0,0 +1,32 @@
+var h = document.createElement("H1"); 
+var t = document.createTextNode("Welcome to my JS site"); // Create a text element 
+h.appendChild(t); // Append the text node to the H1 element 
+ 
+document.body.appendChild(h); // Append the H1 element to the document body 
+
+var newP = document.createElement("p")
+newP.textContent = "All of this was created with Javascript!"
+document.body.append(newP)
+newP.style.textAlign = "center"
+newP.style.fontSize = "30px"
+
+var newLi = documents.createElement("li")
+newLi.textContent = "You can do it"
+document.body.append(li)
+
+var newLi = documents.createElement("li")
+newLi.textContent = "Dedication is the key"
+document.body.append(li)
+
+var newLi = documents.createElement("li")
+newLi.textContent = "Never quit on yourself"
+document.body.append(li)
+
+
+
+
+
+
+
+
+
