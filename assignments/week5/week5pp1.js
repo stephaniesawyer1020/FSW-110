6e1175e65b31b028d63f01f953a5d72ee257f69b
stephaniesawyer1020 <71791053+stephaniesawyer1020@users.noreply.github.com>
2020-12-20 00:34:19 -0700

    Delete week5pp1.js

--- assignments/week5/week5pp1.js
+++
@@ -1,6 +0,0 @@
-function getdetails(){
-    var a = document.forms["details"]["lastname"].value;
-    var b = document.forms["details"]["firstname"].value;
-    var c= document.forms["details"]["email"].value;
-    alert("Your name is: "+a+" "+b+". Your e-mail is: "+c);
-}
